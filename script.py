90263616643867e5fc42651aa4cd36b2315f3975
Anuj-AD <anujdadhwal3012@gmail.com>
2020-04-28 18:45:01 +0530

    Version 1: Running successfully.
    Random pauses and scrolls have been added to ensure the account doesn't get reported as a bot.

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-#code here
 #program to upvote answers on quora using chrome browser
 import webbrowser
 import pyautogui as pag
@@ -6,11 +5,12 @@ import cv2
 import imutils
 import numpy as np
 import time
+from random import randint
 
-url = 'https://www.quora.com/' #our website
+url = 'https://www.instagram.com/' #our website
 path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s' #path of chrome
 webbrowser.get(path).open(url) #open website on chrome
-time.sleep(3) #due to poor connection, webpage may take time to load
+time.sleep(3)
 image = pag.screenshot("ss.png") #take screenshot
 image = cv2.imread("ss.png")
 #cv2.imshow("Screenshot",imutils.resize(image, width = 400)) #to check the screenshot
@@ -19,13 +19,15 @@ logoLocation = pag.locateOnScreen('logo.png', confidence = 0.9)
 logoPoint = pag.center(logoLocation)
 pag.moveTo(logoPoint)
 time.sleep(3)
-timeout = time.time() + 60*5 #5 minute
+timeout = time.time() + 60 #1 minute
 while True:
-    time.sleep(0.5)
+    rand1 = randint(600,800)/1000   
+    time.sleep(rand1)
     print(1)
     if time.time() > timeout: 
         break
-    pag.scroll(-200)
+    rand2 = randint(150,250)
+    pag.scroll(-rand2)
     image = pag.screenshot("ss.png") #take screenshot
     image = cv2.imread("ss.png")
     likeLocation = pag.locateOnScreen('like.png', confidence = 0.9)
@@ -33,3 +35,5 @@ while True:
         likePoint = pag.center(likeLocation)
         pag.moveTo(likePoint)
         pag.click()
+        rand3 = randint(10,30)/30
+        time.sleep(rand3)
